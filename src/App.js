2ce0b074c84b017b9ef20383747ce73094db3ba3
waxhsu <98721786+waxhsu@users.noreply.github.com>
2023-04-20 13:32:33 -0400

    added some info

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Clicker from './components/Clicker/Clicker.js'
 // import Stats from './components/Stats/Stats.js'
-// import Upgrades from './components/Upgrades/Upgrades.js'
+import Upgrades from './components/Upgrades/Upgrades.js'
 
 function App() {
   return (
     <div className="App">
         <Clicker />
         {/* <Stats /> */}
-        {/* <Upgrades /> */}
+        <Upgrades />
     </div>
   );
 }
