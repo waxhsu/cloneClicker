e2def4f61ee3886aa7ac2e0bd17cce2550c73ef1
waxhsu <98721786+waxhsu@users.noreply.github.com>
2023-04-20 13:23:38 -0400

    Update Clicker.js
    removed unnecessary functions

--- src/components/Clicker/Clicker.js
+++ src/components/Clicker/Clicker.js
@@ -1,18 +1,14 @@
 import React, { useState } from 'react' //importing react and useState
+import CLICK from '../../img/billiards_back_animated.gif'
 
 
 export const Clicker = () => {
     const [number, setNumber] = useState(0) //this is an react hook
   return (
-    <div>CLONE CLICKER
-        <div>
-            <button onClick={() => setNumber(number+50)}>PLUS 50</button>
-        <div onClick = {() => setNumber(number+1)}> 
-            {number}
-            </div>
-        <div>
-            <button onClick={() => setNumber(number-50)}>MINUS 50</button></div>
-        </div>
+    <div>
+        <h1>CLONE CLICKER</h1>
+        <img onClick = {() => setNumber(number+1)} src={CLICK} />
+        <h2>{number}</h2>
     </div>
   )
 }
