f3fb7842bfc2952764b318cefbb57567365c0def
waxhsu <98721786+waxhsu@users.noreply.github.com>
2023-09-15 17:03:37 -0700

    not sure if it works

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import { click } from '@testing-library/user-event/dist/click';
 import './App.css';
 import Clicker from './components/Clicker/Clicker.js'
-import Notifications from './components/Notifications/Notifications.js'
+// import Notifications from './components/Notifications/Notifications.js'
 import Roles from './components/Roles/Roles.js'
 import Stats from './components/Stats/Stats.js'
 import Upgrades from './components/Upgrades/Upgrades.js'
 import React, { useState } from 'react'
 
 function App() {
-  const [number, setNumber] = useState(0)
+  const [number, setNumber] = useState(0);
+  const [clickMultiplier, setClickMultiplier] = useState(1);
+  const buyUpgrade = () => {
+    // Implement logic to buy an upgrade and increase the clickMultiplier
+    // For example, you can deduct some cookies and double the clickMultiplier
+    const upgradeCost = 10; // Adjust the cost as needed
+    if (number >= upgradeCost) {
+      setNumber(number - upgradeCost);
+      setClickMultiplier(clickMultiplier * 2);
+    }
+  };
   return (
     <div className="App">
         <Roles />
         
         <div className='rightUI'>
           <Clicker increaseNum={ () => {
-            setNumber (number+1)
+            setNumber (number+clickMultiplier)
           }} />
-          <Stats totalClicks={number} />
+
+
+          <Stats totalClicks={number}/>
+
+
+
           {/* <Notifications /> */}
-          <Upgrades />
+          <Upgrades decreaseNum= { () => {setNumber (number-1)}}/>
         </div>
     </div>
   );
--- src/components/Stats/Stats.js
+++ src/components/Stats/Stats.js
@@ -2,13 +2,13 @@ import React, { useState } from 'react' //importing react and useState
 
 
 
-export const Stats = (props) => {
+export const Stats = (props, money) => {
   const {totalClicks} = props;
   return (
     <div>
       <div className='appStats'>
         <h2>Total Applications: {totalClicks}</h2>
-        <h3>f*cks left to give: {totalClicks*.51}</h3>
+        <h3>f*cks left to give: </h3>
       </div>
 
     </div>
