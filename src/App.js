4ab7b3745603c00e59ff8dd804403b5176b5850e
waxhsu <98721786+waxhsu@users.noreply.github.com>
2023-09-17 11:52:21 -0700

    upgrade mechanic

--- src/App.js
+++ src/App.js
@@ -1,35 +1,13 @@
-// import { click } from '@testing-library/user-event/dist/click';
+import React from 'react';
 import './App.css';
-// import Clicker from './components/Clicker/Clicker.js'
-// import Notifications from './components/Notifications/Notifications.js'
-// import Roles from './components/Roles/Roles.js'
-// import Stats from './components/Stats/Stats.js'
-// import Upgrades from './components/Upgrades/Upgrades.js'
-import React, { useState } from 'react'
-import apply from './img/easyapply.png'
+import Clicker from './components/Clicker/Clicker.js';
 
-const App = () => {
-  const [applications, setApplications] = useState(0);
-
-  const handleClick = () => {
-    setApplications(applications + 1);
-  };
-
+function App() {
   return (
-    <div>
-      <h1>You Got Laid Off</h1>
-      <div className="app-container">
-        <img
-          src={apply} // Replace with the path to your cookie image
-          onClick={handleClick}
-          className="apply"
-        />
-      </div>
-      <div className="application-count">
-        <h1>Total Applications: {applications}</h1>
-      </div>
+    <div className="App">
+      <Clicker />
     </div>
   );
-};
+}
 
 export default App;
--- src/components/Clicker/Clicker.js
+++ src/components/Clicker/Clicker.js
@@ -1,21 +1,51 @@
 import React, { useState } from 'react' //importing react and useState
 import './Clicker.css';
-import CLICK from '../../img/easyapply.png'
+import apply from '../../img/easyapply.png'
 
 
 
-export const Clicker = (increase) => {
-  const {increaseNum} = increase;
+export const Clicker = ( ) => {
+  const [applications, setApplications] = useState(0);
+  const [clickMultiplier, setClickMultiplier] = useState(1);
+
+  // const handleClick = () => {
+  //   setApplications(applications + 1);
+  // };
+
+
+  const handleClick = () => {
+    setApplications(application + clickMultiplier);
+
+  const buyUpgrade = () => {
+    // Implement logic to buy an upgrade and increase the clickMultiplier
+    // For example, you can deduct some cookies and double the clickMultiplier
+    const upgradeCost = 10; // Adjust the cost as needed
+    if (application >= upgradeCost) {
+      setApplications(application - upgradeCost);
+      setClickMultiplier(clickMultiplier * 2);
+    }
+  }
+  
+
+  
+  };
+
   return (
-    <div className='Clicker'>
-        <div className='apply'>
-          <img className= 'easyApply' onClick = {() => increaseNum()} src={CLICK} />
-        </div>
-
-
-
+    <div>
+      <h1>You Got Laid Off</h1>
+      <div className="app-container">
+        <img
+          src={apply} // Replace with the path to your cookie image
+          onClick={handleClick}
+          className="apply"
+        />
+      </div>
+      <div className="application-count">
+        <h1>Total Applications: {applications}</h1>
+      </div>
     </div>
-  )
+  );
 }
+
 
 export default Clicker;
