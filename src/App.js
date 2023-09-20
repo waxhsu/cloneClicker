a51fac21944dbf2a2dd1b8ea648c91b9d47f575e
waxhsu <98721786+waxhsu@users.noreply.github.com>
2023-09-20 00:58:11 -0700

    attempt3

--- src/App.js
+++ src/App.js
@@ -1,32 +1,22 @@
 import React, { useState } from 'react';
 import './App.css';
-import CookieButton from './components/Apply/Apply.js';
-import StatsDisplay from './components/Statsdisplay/Statsdisplay.js'; // Import the new component
-import Autofarm from './components/Autofarm/Autofarm.js'; // Import the new component
 
 function App() {
-  // State to track the number of cookies and currency
+  // State to track the number of cookies
   const [cookies, setCookies] = useState(0);
-  const [currency, setCurrency] = useState(0);
 
   // Function to handle cookie clicking
-  const handleCookieClick = (amount) => {
-    setCookies(cookies + amount);
-    if ((cookies + amount) % 10 === 0) {
-      setCurrency(currency + 1);
-    }
+  const handleCookieClick = () => {
+    // Increase the number of cookies by 1 when clicked
+    setCookies(cookies + 1);
   };
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>Cookie Clicker</h1>
-        {/* Use the StatsDisplay component */}
-        <StatsDisplay cookies={cookies} currency={currency} />
-        {/* Use the CookieButton component */}
-        <CookieButton onClick={() => handleCookieClick(1)} />
-        {/* Use the Autofarm component */}
-        <Autofarm addCookies={handleCookieClick} />
+        <p>Cookies: {cookies}</p>
+        <button onClick={handleCookieClick}>Click Me!</button>
       </header>
     </div>
   );
--- src/const.js
+++ src/const.js
@@ -1,18 +1,37 @@
-export const cookieObjects = [
+// const.js
+const upgrades = [
   {
-    name: 'Cursor',
-    cost: 10,
+    id: 1,
+    name: 'Click Multiplier',
+    cost: 1,
+    clickMultiplier: 2, // Increase the click multiplier by 2
+    description: 'Increase your click power. Clicks generate more cookies.',
   },
   {
+    id: 2,
+    name: 'Cursor',
+    cost: 5,
+    clickRate: 20,
+    description: 'Automatically clicks once per second.',
+    multiplier: 20,
+  },
+  {
+    id: 3,
     name: 'Grandma',
     cost: 50,
+    clickRate: 5,
+    description: 'Grandma bakes cookies for you. +5 cookies per second.',
+    multiplier: 5,
   },
   {
-    name: 'Farm',
-    cost: 100,
-  },
-  {
+    id: 4,
     name: 'Factory',
-    cost: 500,
+    cost: 200,
+    clickRate: 20,
+    description: 'Cookie production is automated. +20 cookies per second.',
+    multiplier: 20,
   },
+  // Add more upgrades as needed
 ];
+
+export default upgrades;
